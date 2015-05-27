cfab12bb7e1575ef6e5f7ad8704083245a1497c8
josedes <josedes@gmail.com>
2015-05-27 12:05:54 -0430

    se agrego una migracion campo nuevo en la tabla frascos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527134310) do
+ActiveRecord::Schema.define(version: 20150527163527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150527134310) do
     t.integer  "usuario_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "estado2"
   end
 
   create_table "mensajes", force: :cascade do |t|
@@ -150,9 +151,8 @@ ActiveRecord::Schema.define(version: 20150527134310) do
     t.integer  "rol"
   end
 
-#  add_index "usuarios", ["email"], name: "index_usuarios_on_email",  using: :btree
-  add_index "usuarios", ["username"], name: "index_usuarios_on_username", unique:true, using: :btree
   add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
+  add_index "usuarios", ["username"], name: "index_usuarios_on_username", unique: true, using: :btree
 
   add_foreign_key "menu0s", "contenidos"
   add_foreign_key "menu1s", "contenidos"
